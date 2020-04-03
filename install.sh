f1bce344f940a2dbdfc8a681c97dd8703659bc02
Jing Dong <jingdong0@gmail.com>
2020-04-02 23:04:07 -0400

    Fix install.sh for conda
    * Remove ta-lib from the requirement, as it requires sudo for installation
    * Add parallel build and ccache support for building extensions.
    
    Signed-off-by: Jing Dong <jingdong0@gmail.com>

--- install.sh
+++ install.sh
@@ -25,13 +25,13 @@ function ta-lib-exists()
 {
     ta-lib-config --libs > /dev/null
 }
-ta-lib-exists || install-ta-lib
+# ta-lib-exists || install-ta-lib
 
 # old versions of ta-lib imports numpy in setup.py
 $python -m pip install numpy
 
 # Install extra packages
-$python -m pip install ta-lib
+# $python -m pip install ta-lib
 $python -m pip install https://vnpy-pip.oss-cn-shanghai.aliyuncs.com/colletion/ibapi-9.75.1-py3-none-any.whl
 
 # Install Python Modules
@@ -41,4 +41,4 @@ $python -m pip install -r requirements.txt
 locale-gen zh_CN.GB18030
 
 # Install vn.py
-$python -m pip install . $@
+CC="ccache gcc" CXX="ccache g++" $python -m pip install -e . $@
